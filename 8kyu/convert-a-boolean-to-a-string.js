1ed3201cc100ca866dce3df2c5e64eced9c70658
Christiana Ukoli <cukoli22@gmail.com>
2022-06-26 22:31:37 -0500

    adding new codewars challenge

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,13 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+    //if the boolean is true, the string should be true. if not, it's false
+    if (b === true){
+      return "true"
+    } else {
+      return "false"
+    }
+  }

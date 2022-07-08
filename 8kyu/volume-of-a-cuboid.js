35e0b3ff050bc6dcb94b68717f469180996d9226
Christiana Ukoli <cukoli22@gmail.com>
2022-07-07 21:06:58 -0500

    adding new codewars challenge

---
+++ 8kyu/volume-of-a-cuboid.js
@@ -0,0 +1,11 @@
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // use the volume formula - multiply all values and return
+      console.log(length * width * height) //console.log for testing
+      return length * width * height
+    }
+  }

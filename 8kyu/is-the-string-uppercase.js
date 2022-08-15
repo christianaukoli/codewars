70534086c38cb8d3dd58fe71cee5f54562fcfaf5
Christiana Ukoli <cukoli22@gmail.com>
2022-08-14 23:17:18 -0500

    adding new codewars challenge

---
+++ 8kyu/is-the-string-uppercase.js
@@ -0,0 +1,29 @@
+/*Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+
+
+
+
+String.prototype.isUpperCase = function() {
+    // search for lowercase
+    console.log(this)
+    console.log(this.toUpperCase())
+    //write if statement for if lowercase is found
+    if(this == this.toUpperCase()){
+      return true
+    } //write statement if lowercase isn't found
+      else{
+      return false
+    }
+    
+  }

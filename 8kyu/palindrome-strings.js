8ebf04d24d1756aeed51c981677e7d25a6c5dd9e
Christiana Ukoli <cukoli22@gmail.com>
2022-08-27 17:25:24 -0500

    adding new codewars challenge

---
+++ 8kyu/palindrome-strings.js
@@ -0,0 +1,26 @@
+/*Palindrome strings
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+Examples(Input ==> Output)
+"anna"   ==> true
+"walter" ==> false
+12321    ==> true
+123456   ==> false
+*/
+
+
+
+
+function isPalindrome(line) {
+    //check value
+    console.log(line);
+    //if reverse = forward, return true
+   const str = line.toString();
+    if(str === str.split('').reverse().join('')){
+      return true
+    }else{
+      return false
+    }
+  }

a63b227e6d96acf652ab7aa2af2e86216ba07d61
Christiana Ukoli <cukoli22@gmail.com>
2022-10-10 13:38:32 -0500

    adding new codewars challenge

---
+++ 8kyu/define-a-card-suit.js
@@ -0,0 +1,27 @@
+/*You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+
+
+
+function defineSuit(card) {
+    // console.log to check value
+    console.log(card)
+    //write if statement for all conditions
+    if(card.includes("♣")){
+      return "clubs"
+    } else if (card.includes("♦")){
+      return "diamonds"
+    } else if (card.includes("♥")){
+      return "hearts"
+    }else{
+      return "spades"
+    }
+  }

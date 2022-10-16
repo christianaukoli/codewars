da246da5a3a2038399086bdec5604875b0002e67
Christiana Ukoli <cukoli22@gmail.com>
2022-10-16 17:35:39 -0500

    adding new codewars challenge

---
+++ 8kyu/take-an-arrow-to-the-knee-functionally.js
@@ -0,0 +1,26 @@
+/*Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+*/
+
+
+
+
+var ArrowFunc = function(arr) {
+    //check values
+    console.log(arr)
+    //convert to ASCII letters using fromCharCode
+    let words = ""
+    for(let i=0; i<arr.length; i++){
+    words = arr.map(code => String.fromCharCode(code)).join(''); //Complete this function
+    }
+    return words
+  }

66f21be6cb3316b69dca784202541f94e885e144
Christiana Ukoli <cukoli22@gmail.com>
2022-10-22 18:57:05 -0500

    adding new codewars challenge

---
+++ 8kyu/grasshopper-terminal-game-move-function.js
@@ -0,0 +1,21 @@
+/*Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15
+*/
+
+
+
+
+function move (position, roll) {
+    // determine the final position of the player
+    //show value
+    console.log(position, roll)
+    //add 2 dice rolls to position
+    let final = position + 2*roll
+    //return
+    return final
+  }

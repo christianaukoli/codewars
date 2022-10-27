d36857057e8af2e81da49a425201536836217157
Christiana Ukoli <cukoli22@gmail.com>
2022-10-26 19:54:39 -0500

    adding new codewars challenge

---
+++ 8kyu/difference-of-volumes-of-cuboids.js
@@ -0,0 +1,24 @@
+/*In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.
+
+If you can, try writing it in one line of code.
+*/
+
+
+
+function findDifference(a, b) {
+    //find the difference between a and b volumes and try to write it in one line
+    return Math.abs(a.reduce((a,b)=>a*b) - b.reduce((a,b)=>a*b))
+    
+  //   //show value
+  //   console.log(a,b)
+  //   //find volumes
+  //   let volumeA= a.reduce((a,b)=>a*b)
+  //   let volumeB= b.reduce((a,b)=>a*b)
+  //   //return difference
+  //   console.log(volumeA - volumeB)
+  //   return Math.abs(volumeA - volumeB)
+  }

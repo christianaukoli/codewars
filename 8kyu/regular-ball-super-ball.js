f379121cf15dde45257a50cc8c3cb085a5a23bd9
Christiana Ukoli <cukoli22@gmail.com>
2022-11-01 19:08:49 -0500

    adding new codewars challenge

---
+++ 8kyu/regular-ball-super-ball.js
@@ -0,0 +1,25 @@
+/*Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+
+
+
+var Ball = function(ballType) {
+    // figure out how to instantiate
+    //create balltype argument
+    if(!ballType){
+      this.ballType = "regular"
+    } else{
+      this.ballType = "super"
+    }
+    
+  };
+  

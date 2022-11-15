d825c0670d963acc8dd174289dbafeddde9fd8db
Christiana Ukoli <cukoli22@gmail.com>
2022-11-14 19:12:12 -0600

    adding new codewars challenge

---
+++ 8kyu/hex-to-decimal.js
@@ -0,0 +1,11 @@
+//Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+
+function hexToDec(hexString){
+    //convert hex to decimal
+    //statement
+    let dec = parseInt(hexString, 16)
+    //return
+    return dec
+  }

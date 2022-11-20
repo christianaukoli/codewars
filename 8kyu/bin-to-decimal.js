509904277c48ad761ee8760367bba54b4b0ded8d
Christiana Ukoli <cukoli22@gmail.com>
2022-11-19 21:46:08 -0600

    adding new codewars challenge

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,8 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+function binToDec(bin){
+    // convert binary to decimal & return
+    let num = parseInt(bin,2)
+    return num
+  }

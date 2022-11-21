5402ba5b08ee98b976473efc11c11dcd6a861013
Christiana Ukoli <cukoli22@gmail.com>
2022-11-20 22:07:20 -0600

    adding new codewars challenge

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,16 @@
+/*Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+
+// function main [verb, noun]
+// original code
+
+//   return verb + noun
+// }
+
+function main (verb, noun){
+    //find syntax mistakes so function works
+    
+      return verb + noun
+    }
